26b69dcb6680c0384ff55dddb89859a4839771b9
Bautista Poli <bautistapoli2004@gmail.com>
2025-08-08 23:15:42 -0300

    Se siguio cambiando el reactive de la tabla

--- src/app/Pages/InicioComponentes/photo-displayer/photo-displayer.component.ts
+++ src/app/Pages/InicioComponentes/photo-displayer/photo-displayer.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CarouselModule } from 'ngx-owl-carousel-o';
-import { OwlOptions } from 'ngx-owl-carousel-o';
+import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'photo-displayer',
@@ -11,24 +10,33 @@ import { OwlOptions } from 'ngx-owl-carousel-o';
   styleUrl: './photo-displayer.component.css'
 })
 export class PhotoDisplayerComponent {
+  images = [
+    'assets/OMNIA-SLIDER2-1.png',
+    'assets/OMNIA-SLIDER3-1.png',
+    'assets/OMNIA-SLIDER1-1.png',
+  ];
+
+  ready = false;
+
   customOptions: OwlOptions = {
-    loop: true,
+    loop: this.images.length > 1,   // evita clones si hay 1 sola
     mouseDrag: true,
     touchDrag: true,
     pullDrag: false,
-    dots: false,
+    dots: true,
+    nav: true,
     navSpeed: 700,
     autoWidth: false,
     navText: ['<-', '->'],
     responsive: {
       0: { items: 1 },
-      740: { items: 3 },
+      740: { items: 1 },
       940: { items: 1 }
     },
-    nav: true,
     autoplay: true,
     autoplayTimeout: 3000,
     autoplayHoverPause: true,
-    smartSpeed: 1000
+    smartSpeed: 600
   };
 }
+
